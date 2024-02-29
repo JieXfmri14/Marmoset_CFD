617a24952703c66f3ca7cdf15c0689188bdacc95
fMRI_Network <2359982955@qq.com>
2024-02-29 22:59:00 +0800

    Update CFD_FullPipeline_demo.m

--- CFD_FullPipeline_demo.m
+++ CFD_FullPipeline_demo.m
@@ -2,7 +2,7 @@ clc
 clear all
 
 % set code folder path
-mypath='E:\BaiduSyncdisk\AMatlab_code\A_RfMRI_Aanlysis\Marmoset_CFD_301\'; 
+mypath='E:\Marmoset_CFD-main\'; 
 addpath(genpath(strcat(mypath,'functions_matlab')))
 
 %% Step1: compute and decompose Graph Laplacian 
